bb869a91a10c18b31e78c0cdb58a851b0179ebd7
Michael Sparks <95430933+Main-Michael-Sparks@users.noreply.github.com>
2022-01-17 17:24:44 -0500

    Add files via upload
    Changed Polybius to include two new helper functions(_isNum and _twoDigitNumSplit); cleaned up the code and added additional code comments.  Added index.html

--- src/polybius.js
+++ src/polybius.js
@@ -6,62 +6,63 @@
 const polybiusModule = (function () {
   // you can add any code you want within this function scope
 
-  function polybius(input, encode = true) {
-    //below are function scope var declarations, 1 object, two arrays and 'is a number check'
-    const alphabet = {
-      a: "11",
-      b: "21",
-      c: "31",
-      d: "41",
-      e: "51",
-      f: "12",
-      g: "22",
-      h: "32",
-      "i/j": "42",
-      k: "52",
-      l: "13",
-      m: "23",
-      n: "33",
-      o: "43",
-      p: "53",
-      q: "14",
-      r: "24",
-      s: "34",
-      t: "44",
-      u: "54",
-      v: "15",
-      w: "25",
-      x: "35",
-      y: "45",
-      z: "55",
-    };
-    const userInput = [];
-    const outputArr = [];
-    const inputCheck = Number.isNaN(Number(input.split(" ").join("")));
+  function _isNum(numString) {
+    //checks to see if a string is made up of numbers or not
 
-    if (!inputCheck && input.split(" ").join("").length % 2 != 0) {
-      //check if the number input is even or not.
-      return false;
-    }
-    //Check if input string is 'letters' or numbers.
-    if (inputCheck) {
-      userInput.push(...input.toLowerCase().split("")); //if the input string is 'letters', put each letter into the user input array
-    } else {
-      // if the input string is 'numbers' split the numbers into pairs (two digits) and put each pair into an array element.
-      let numTransferVar = "";
-      for (let i = 0; i < input.length; i++) {
-        if (input[i] === " ") {
-          userInput.push(" ");
-        } else {
-          numTransferVar += input[i];
-          if (numTransferVar.length == 2) {
-            userInput.push(numTransferVar);
-            numTransferVar = "";
-          }
+    //reversing the NaN bool to make it less counter-intuitive
+    return Number.isNaN(Number(numString.split(" ").join(""))) ? false : true;
+  }
+
+  function _twoDigitNumSplit(numString) {
+    //splits a string into two character segments, and puts those segments into an array.
+    
+    //set output array, and a container for holding the two digit string segments
+    const doubleDigitArray = [];
+    let doubleDigitContainer = "";
+    //loop through the 'numString' by length.
+    for (let i = 0; i < numString.length; i++) {
+      //if the 'numString' arg contains spaces, preserve those spaces.
+      if (numString[i] === " ") {
+        doubleDigitArray.push(" ");
+        //if the 'numString' string current index is not a space, put that character into the digit container
+      } else if (numString[i] !== " ") {
+        doubleDigitContainer += numString[i];
+        //if the 'doubleDigitContainer' has two num characters; put the pair into the digit array and reset the digit container.
+        if (doubleDigitContainer.length == 2) {
+          doubleDigitArray.push(doubleDigitContainer);
+          doubleDigitContainer = "";
         }
       }
     }
-    //if encode or decode is set, loop through and set the corresponding key:value pair. 
+    return doubleDigitArray;
+  }
+
+  function polybius(input, encode = true) {
+    //early close if a 'number string' input is not of even length.
+    if (_isNum(input) && input.split(" ").join("").length % 2 != 0) {
+      return false;
+    }
+
+    //alphabet object, user input array and output array.
+    const alphabet = {
+      a: "11", b: "21", c: "31", d: "41", e: "51",
+      f: "12", g: "22", h: "32", "i/j": "42", k: "52",
+      l: "13", m: "23", n: "33", o: "43", p: "53",
+      q: "14", r: "24", s: "34", t: "44", u: "54",
+      v: "15", w: "25", x: "35", y: "45", z: "55",
+    };
+
+    const userInput = [];
+    const outputArr = [];
+
+    //checks to see if the input is made of letter characters or digits; if digits runs them through a spliting process.
+    if (!_isNum(input)) {
+      userInput.push(...input.toLowerCase().split(""));
+    } else if (_isNum(input)) {
+      userInput.push(..._twoDigitNumSplit(input));
+    }
+
+    //if encode or decode is set, loop through and set the corresponding key:value pair.
     userInput.forEach((element) => {
       if (encode) {
         for (let key in alphabet) {
@@ -97,4 +98,3 @@ const polybiusModule = (function () {
 })();
 
 module.exports = { polybius: polybiusModule.polybius };
-
